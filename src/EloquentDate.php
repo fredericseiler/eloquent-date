7c3b953e9956dec5a30c7c42a58e5f0099a9a5d4
Frederic Seiler <frederic@seiler.io>
2017-06-23 00:58:55 +0200

    5.4

--- src/EloquentDate.php
+++ src/EloquentDate.php
@@ -55,13 +55,15 @@ trait EloquentDate
         // If the value is in simply year, month, day format, we will instantiate the
         // Date instances from that format. Again, this provides for simple date
         // fields on the database, while still supporting Datized conversion.
-        if (preg_match('/^(\d{4})-(\d{1,2})-(\d{1,2})$/', $value)) {
+        if ($this->isStandardDateFormat($value)) {
             return Date::createFromFormat('Y-m-d', $value)->startOfDay();
         }
 
         // Finally, we will just assume this date is in the format used by default on
         // the database connection and use that format to create the Date object
         // that is returned back out to the developers after we convert it here.
-        return Date::createFromFormat($this->getDateFormat(), $value);
+        return Date::createFromFormat(
+            $this->getDateFormat(), $value
+        );
     }
 }
